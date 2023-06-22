b4c6da8770e2649d7c809c6853581c693c9265c9
Islombek <islombobokulov143@gmail.com>
2023-06-22 21:57:38 +0500

    Translate

--- WordGenius/windows/Words/WordsCreateWindow.xaml.cs
+++ WordGenius/windows/Words/WordsCreateWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WordGenius.windows.Words;
 
-namespace WordGenius.Pages
+namespace WordGenius.windows.Words
 {
     /// <summary>
-    /// Interaction logic for WordsPage.xaml
+    /// Interaction logic for WordsCreateWindow.xaml
     /// </summary>
-    public partial class WordsPage : Page
+    public partial class WordsCreateWindow : Window
     {
-        public WordsPage()
+        public WordsCreateWindow()
         {
             InitializeComponent();
-        }
-
-        private void btnCreate_Click(object sender, RoutedEventArgs e)
-        {
-            WordsCreateWindow wordsCreateWindow = new WordsCreateWindow();
-            wordsCreateWindow.ShowDialog();
         }
     }
 }
