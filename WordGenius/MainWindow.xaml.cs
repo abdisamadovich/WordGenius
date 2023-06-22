5939a966266084ee51fdd8800586d069b21a3572
Islombek <islombobokulov143@gmail.com>
2023-06-22 16:49:06 +0500

    Ui tugirladim

--- WordGenius/MainWindow.xaml.cs
+++ WordGenius/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WordGenius.Pages;
 
 namespace WordGenius
 {
@@ -52,6 +53,37 @@ namespace WordGenius
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void rbHome_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage homePage = new HomePage();
+            PageNavigator.Content=homePage;
+
+        }
+
+        private void rbWords_Click(object sender, RoutedEventArgs e)
+        {
+            WordsPage wordsPage = new WordsPage();
+            PageNavigator.Content=wordsPage;
+        }
+
+        private void rbTest_Click(object sender, RoutedEventArgs e)
+        {
+            TestPage testPage = new TestPage();
+            PageNavigator.Content=testPage;
+        }
+
+        private void rbTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            TranslatePage translatePage = new TranslatePage();
+            PageNavigator.Content=translatePage;
+        }
+
+        private void rbAbout_Click(object sender, RoutedEventArgs e)
+        {
+            AboutPage aboutPage = new AboutPage();
+            PageNavigator.Content=aboutPage;
         }
     }
 }
